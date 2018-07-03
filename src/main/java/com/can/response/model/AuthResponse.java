a2a7b9bd3fccbc3ef450451f3e1b1694a79ddef9
LCN <1455476411@qq.com>
2018-07-03 17:15:31 +0800

    正则工具类和声明工具类

---
+++ src/main/java/com/can/response/model/AuthResponse.java
@@ -0,0 +1,20 @@
+package com.can.response.model;
+
+/**
+ * @description: 权限相关响应类
+ *
+ * @author: LCN
+ * @date: 2018-06-14 23:34
+ */
+public class AuthResponse {
+
+	private String errorMsg;
+
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
+}
